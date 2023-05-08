2e7b9219e65c8902df8a3f975e40035800cab982
Evan Vinciguerra <evanv511@gmail.com>
2023-05-08 17:11:55 -0400

    before dinner

--- src/App.tsx
+++ src/App.tsx
@@ -15,20 +15,67 @@ const reorder = (
   endIndex: number
 ): Array<Item> => {
   const result = Array.from(list);
+  const og = Array.from(list);
+
   const [removed] = result.splice(startIndex, 1);
   removed['lastUpdated'] = new Date();
-  removed['changeAmount'] = '' + (startIndex - endIndex);
+
+  let change = startIndex - endIndex;
+  if (
+    (change < 0 && (removed['changeAmount'] || 0) < 0) ||
+    (change > 0 && (removed['changeAmount'] || 0) > 0)
+  ) {
+    // we want to further decrease/increase
+    removed['changeAmount'] += change;
+  } else {
+    removed['changeAmount'] = change;
+  }
+
+  console.log(startIndex, endIndex);
+  // let pre2 = result.slice(endIndex);
+  // console.log(pre2);
 
   result.splice(endIndex, 0, removed);
 
+  if (startIndex < endIndex) {
+    // increase all that were moved above the currently selected by one
+    for (let i = 0; i < endIndex; i++) {
+      result[i].changeAmount += 1;
+    }
+  } else {
+    // decrease all that were moved below the currently select by one
+    for (let i = endIndex; i < startIndex; i++) {
+      result[i].changeAmount -= 1;
+    }
+  }
+
   return result;
+};
+
+const getArrow = (change: number) => {
+  // ▲▼
+  let arrow: string = '';
+  let cls: string = '';
+
+  if (change > 0) {
+    arrow = '▲';
+    cls = 'increase';
+  } else if (change < 0) {
+    arrow = '▼';
+    cls = 'decrease';
+  }
+
+  return (
+    <span className={`change ${cls}`}>
+      {arrow} {change === 0 ? '-' : Math.abs(change)}
+    </span>
+  );
 };
 
 type Item = {
   id: string;
   name: string;
-  changeDirection?: string;
-  changeAmount?: string;
+  changeAmount: number;
   lastUpdated?: Date;
   createdAt: Date;
 };
@@ -74,6 +121,7 @@ class App extends React.Component<MyProps, MyState> {
       id: uuidv4(),
       name: typedItem,
       createdAt: new Date(),
+      changeAmount: 0,
     };
 
     items.push(item);
@@ -141,7 +189,7 @@ class App extends React.Component<MyProps, MyState> {
                         <div className='name'>{item.name}</div>
                         {this.state.adhdMode && (
                           <div className='amount'>
-                            {item.changeDirection} {item.changeAmount}
+                            {getArrow(item.changeAmount)}
                           </div>
                         )}
                         <div className='remove'>x</div>
